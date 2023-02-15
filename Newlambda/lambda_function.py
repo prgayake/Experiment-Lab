154461a6d77415269b4e5a8c4af513394b00190c
Pradyumna Gayake <55043418+prgayake@users.noreply.github.com>
2023-02-15 09:16:06 +0000

    Done lambda intergartion

--- Newlambda/lambda_function.py
+++ Newlambda/lambda_function.py
@@ -1,7 +1,13 @@
 import json
 import pandas as pd
+import boto3
 
+client = boto3.client('dynamodb')
 def lambda_handler(event, context):
-    
-    return 'Hi, second lambda function'
+    # get all items from dynamodb
+    response = client.scan(
+        TableName='apidata'
+    )
+
+    return response
     
