273ebe93f955107da284d21997f204b0042ded3c
Pradyumna Gayake <55043418+prgayake@users.noreply.github.com>
2023-02-16 04:03:37 +0000

    update

--- lambda_function1.py
+++ lambda_function1.py
@@ -5,13 +5,13 @@ import boto3
 client = boto3.client('dynamodb')
 def lambda_handler(event, context):
     #get id from query string and find in dynamodb table
-    id = event['queryParams']['id']
+    ID= event['queryParams']['id']
     print(event)
     response = client.get_item(
         TableName='test',
         Key={
             'id': {
-                'S': id
+                'S': ID
             }
         }
     )
