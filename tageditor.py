2c38c06b1992bd3cefa2540c8b6752f037698783
Pradyumna Gayake <55043418+prgayake@users.noreply.github.com>
2023-03-14 09:42:58 +0000

    update tag

--- tageditor.py
+++ tageditor.py
@@ -1,11 +1,5 @@
 import boto3
+import pandas as pd
 
-#get the data from text file
-def updatetags():
-    with open('data.txt', 'r') as f:
-        instance_ids = f.read()
-    ec2 = boto3.resource('ec2')
-    for i in range (0,len(instance_ids)):
-        instance = ec2.Instance(instance_ids[i])
-        instance.create_tags(Tags=[{'Key': 'Name', 'Value': 'test'}])
-        print(instance_ids[i])
+def update_tags():
+    # 
