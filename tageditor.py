8e2c4e7c35d7ab8880bb9a27def75a3bc4c0ca0e
Pradyumna Gayake <55043418+prgayake@users.noreply.github.com>
2023-03-14 10:23:30 +0000

    update

--- tageditor.py
+++ tageditor.py
@@ -1,5 +1,24 @@
 import boto3
-import pandas as pd
+import openpyxl
 
 def update_tags():
-    # 
+    workbook = openpyxl.load_workbook('ec2data.xlsx')
+    worksheet = workbook['Sheet1']
+    column1 = worksheet['A']
+    column2 = worksheet['B']
+    column3 = worksheet['C']
+
+
+    for i in range(0, len(column1)):
+        ec2 = boto3.resource('ec2')
+        instance = ec2.Instance(column1[i].value)
+        instance.create_tags(
+            Tags=[
+                {
+                    'project_Code':column3[i].value,
+                    'Environment': column2[i].value,
+                },
+            ]
+        )
+    
+update_tags()
